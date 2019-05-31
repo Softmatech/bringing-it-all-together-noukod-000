89f789dc6ba8e17f2825d8d03fd9cec0e0f81fa2
Andy Feidje Joseph <ingandy75@gmail.com>
2019-05-31 16:08:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,5 +6,15 @@ class Dog
     @id,@name, @breed = attr[:id],attr[:name], attr[:breed]
   end
 
+  def self.create_table
+      sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs(
+        id INTEGER PRIMARY KEY ,
+        name TEXT, breed TEXT
+      )
+      SQL
+
+      DB[:conn].execute(sql)
+    end
 
 end
