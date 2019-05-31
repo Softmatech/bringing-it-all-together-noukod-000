8a65f094968abe7c1d940431b26ac50672b188a0
Andy Feidje Joseph <ingandy75@gmail.com>
2019-05-31 15:57:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
   attr_accessor :id, :name, :breed
 
   def initialize **attr
-    @name, @breed, @id = attr[:name], attr[:breed], attr[:id]
+    @id,@name, @breed = attr[:name], attr[:breed], attr[:id]
   end
 
 
